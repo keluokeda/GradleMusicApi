3cc6a2f277b2b1294a3e54557ec8f9bddc74beed
ke <913918146@qq.com>
2026-05-31 22:52:14 +0800

    1，歌手列表。

--- src/main/kotlin/com/ke/gradlemusicapi/controller/ArtistController.kt
+++ src/main/kotlin/com/ke/gradlemusicapi/controller/ArtistController.kt
@@ -3,7 +3,9 @@ package com.ke.gradlemusicapi.controller
 
 import com.ke.gradlemusicapi.api.HttpService
 import com.ke.gradlemusicapi.cookie
+import com.ke.gradlemusicapi.entity.response.Artist
 import com.ke.gradlemusicapi.entity.vo.ArtistDetailVO
+import com.ke.gradlemusicapi.entity.vo.BaseListVO
 import com.ke.gradlemusicapi.entity.vo.BaseVO
 import com.ke.gradlemusicapi.entity.vo.MvVO
 import io.swagger.v3.oas.annotations.Operation
@@ -22,44 +24,59 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ArtistController(private val httpService: HttpService) {
 
-	@Operation(summary = "获取歌手详情")
-	@SecurityRequirement(name = "Bearer Authentication")
-	@GetMapping("/artist/{id}")
-	suspend fun getArtistDetail(
-		@Parameter(description = "歌手id", required = true, example = "10577")
-		@PathVariable id: Long, authentication: Authentication
-	) = withContext(Dispatchers.IO) {
-		val cookie = authentication.cookie
-		val artist = async {
-			val response = httpService.getArtists(id, cookie)
-			response.artist to response.hotSongs
-		}.await()
+
+    @Operation(summary = "获取歌手列表")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @GetMapping("/artists")
+    suspend fun artists(
+        type: Int = -1,
+        area: Int = -1,
+        index: Int = 1,
+        size: Int = 20,
+        authentication: Authentication
+    ): BaseListVO<Artist> {
+        val response = httpService.getArtistList(type, area, size, (index-1)*size, authentication.cookie)
+        return BaseListVO(data = response.artists, hasMore = response.more)
+    }
+
+    @Operation(summary = "获取歌手详情")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @GetMapping("/artist/{id}")
+    suspend fun getArtistDetail(
+        @Parameter(description = "歌手id", required = true, example = "10577")
+        @PathVariable id: Long, authentication: Authentication
+    ) = withContext(Dispatchers.IO) {
+        val cookie = authentication.cookie
+        val artist = async {
+            val response = httpService.getArtists(id, cookie)
+            response.artist to response.hotSongs
+        }.await()
 
 
-		val artistDesc = async {
-			httpService.getArtistDesc(id, cookie)
-		}
-		val simiArtists = async {
-			httpService.getSimiArtist(id, authentication.cookie)
-		}
-		val mvs = async {
-			httpService.getArtistMv(id, 1000, cookie)
-		}
-		val albums = async {
-			httpService.getArtistAlbums(id, 1000, cookie)
-		}
+        val artistDesc = async {
+            httpService.getArtistDesc(id, cookie)
+        }
+        val simiArtists = async {
+            httpService.getSimiArtist(id, authentication.cookie)
+        }
+        val mvs = async {
+            httpService.getArtistMv(id, 1000, cookie)
+        }
+        val albums = async {
+            httpService.getArtistAlbums(id, 1000, cookie)
+        }
 
-		return@withContext BaseVO.success(
-			ArtistDetailVO(
-				artist.first,
-				artist.second,
-				artistDesc.await(),
-				simiArtists.await().artists,
-				mvs.await().mvs.map {
-					MvVO(it.id, it.name, it.image)
-				},
-				albums.await().hotAlbums
-			)
-		)
-	}
+        return@withContext BaseVO.success(
+            ArtistDetailVO(
+                artist.first,
+                artist.second,
+                artistDesc.await(),
+                simiArtists.await().artists,
+                mvs.await().mvs.map {
+                    MvVO(it.id, it.name, it.image)
+                },
+                albums.await().hotAlbums
+            )
+        )
+    }
 }
